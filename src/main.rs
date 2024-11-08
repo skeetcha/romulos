8ebf0fc77dcb66eb24de4d026d63774b8c16e4e3
Cass Unterholzner <cityboundforest@gmail.com>
2024-11-07 22:11:45 -0600

    Adding VGA Text Mode tutorial

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,16 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
