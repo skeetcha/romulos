8c15233650428257462d171f6b90528ede689cf3
Cass Unterholzner <cityboundforest@gmail.com>
2024-11-09 09:37:55 -0600

    Adding Introduction to Paging

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,9 +1,9 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use crate::{gdt, println};
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin;
-use crate::print;
+use crate::{print, hlt_loop};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -19,6 +19,8 @@ lazy_static! {
 
         idt[InterruptIndex::Keybord.as_usize()]
             .set_handler_fn(keyboard_interrupt_handler);
+
+        idt.page_fault.set_handler_fn(page_fault_handler);
 
         idt
     };
@@ -75,6 +77,16 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
     unsafe {
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Keybord.as_u8());
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(stack_frame: InterruptStackFrame, error_code: PageFaultErrorCode) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 #[test_case]
